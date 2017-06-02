3528a8a2f337727343e1094dbf1036f73628e340
Juan Pablo GD <juanpablogarzond@gmail.com>
2017-06-02 17:21:28 -0500

    Redireccionamiento Ok

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -54,9 +54,8 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; SAGA <?= date('Y') ?></p>
 
-        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
